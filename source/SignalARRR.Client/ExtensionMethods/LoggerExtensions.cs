fd9eaab7ada961594a510a916c1ef11d3d127b13
Bernhard Windisch <bernhard.windisch@doob.at>
2020-10-06 20:49:03 +0200

    LoggerExtensions was missing

---
+++ source/SignalARRR.Client/ExtensionMethods/LoggerExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace SignalARRR.Client.ExtensionMethods {
+
+    
+    public static class LoggerExtensions {
+
+        public static ILoggerProvider AsLoggerProvider(this ILogger logger) {
+            return new ExistingLoggerProvider(logger);
+        }
+
+        private class ExistingLoggerProvider : ILoggerProvider {
+            public ExistingLoggerProvider(ILogger logger) {
+                _logger = logger;
+            }
+
+            public ILogger CreateLogger(string categoryName) {
+                return _logger;
+            }
+
+            public void Dispose() {
+                return;
+            }
+
+            private readonly ILogger _logger;
+        }
+    }
+}
